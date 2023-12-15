74fbb9f8e23d6f91713869416c4190614b39f71e
KurosakiTG <142395917+KurosakiTG@users.noreply.github.com>
2023-12-15 10:54:14 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -89,7 +89,7 @@ wget https://${akbarvpnnnnnnnnn}/edu.sh && chmod +x edu.sh && ./edu.sh
 # Ohp Server
 wget https://${akbarvpnnnnnnnnnn}/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 # Install SlowDNS
-wget https://raw.githubusercontent.com/kurosakitg/autoset/master/SLDNS/install-sldns && chmod +x install-sldns && ./install-sldns
+wget https://raw.githubusercontent.com/NevermoreSSH/hop/main/SLDNS/install-sldns && chmod +x install-sldns && ./install-sldns
 # Informasi IP Saya dan Semua Port TCP UDP
 # wget https://raw.githubusercontent.com/kurosakitg/autoset/master/ipsaya.sh && chmod +x ipsaya.sh
 #
